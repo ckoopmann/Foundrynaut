89d664ce0a88538248c55b70a5b4f69996af22e6
Christian Koopmann <christian@MTMAC0057.local>
2022-07-21 11:02:56 +0800

    Make shell files bash compatible

--- run_your_solution.sh
+++ run_your_solution.sh
@@ -1,4 +1,4 @@
-PROBLEM_NAME=("$1") # save positional arg
+PROBLEM_NAME=$1 # save positional arg
 shift # past argument
 forge script src/${PROBLEM_NAME}/YourSolution/SolutionScript.sol:SolutionScript --fork-url=$RPC_URL --private-key=$PRIVATE_KEY $@
 

